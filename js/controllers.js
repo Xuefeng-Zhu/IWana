04f6485b47dd625c20027a807b1c956c9a0d7435
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-09-20 00:00:06 -0500

    add wana but item modal

--- js/controllers.js
+++ js/controllers.js
@@ -3,20 +3,37 @@
 /* Controllers */
 
 var url = "https://iwana.firebaseio.com";
+var ref = new Firebase(url);
 angular.module('myApp.controllers', ['firebase'])
-.controller('userCtrl', ["$scope", "$rootScope", "$firebase", "$firebaseSimpleLogin", function($scope, $rootScope, $firebase,$firebaseSimpleLogin) {
-	
-	//use for user logining 
-	var ref = new Firebase(url + "/Users");
-	$rootScope.auth = $firebaseSimpleLogin(ref);
+    .controller('userCtrl', ["$scope", "$rootScope", "$firebase", "$firebaseSimpleLogin",
+        function($scope, $rootScope, $firebase, $firebaseSimpleLogin) {
 
-	//function to run when user logout 
-	$scope.logout = function(){
-		$rootScope.auth.$logout();
-		$location.path("");
-	}
-	//upload user information when first visit and create message listener 
-}])
-.controller('BuyCtrl', ["$scope", "$rootScope", "$firebase", function($scope, $rootScope, $firebase){
-	
-}]);
+            //use for user logining 
+            $rootScope.auth = $firebaseSimpleLogin(ref);
+
+            //function to run when user logout 
+            $scope.logout = function() {
+                $rootScope.auth.$logout();
+                $location.path("");
+            }
+            //upload user information when first visit and create message listener 
+        }
+    ])
+    .controller('BuyCtrl', ["$scope", "$rootScope", "$firebase",
+        function($scope, $rootScope, $firebase) {
+            $scope.items = $firebase(ref.child("buy"));
+
+            $scope.addItem = function() {
+                $('#buy')
+                    .modal('setting', {
+                        closable: true,
+                        onDeny: function() {},
+                        onApprove: function() {
+                        	
+                        }
+                    })
+                    .modal('show');
+
+            }
+        }
+    ]);
