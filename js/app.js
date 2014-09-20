2ac4e610581676020de41ef911e01c01e4b67464
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-09-19 22:42:45 -0500

    add resource

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/buy', {templateUrl: 'partials/buy.html', controller: 'BuyCtrl'});
+  $routeProvider.when('/sell', {templateUrl: 'partials/sell.html', controller: 'SellCtrl'});
+  $routeProvider.otherwise({redirectTo: '/buy'});
 }]);
--- js/controllers.js
+++ js/controllers.js
@@ -2,10 +2,21 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+var url = "https://iwana.firebaseio.com";
+angular.module('myApp.controllers', ['firebase'])
+.controller('userCtrl', ["$scope", "$rootScope", "$firebase", "$firebaseSimpleLogin", function($scope, $rootScope, $firebase,$firebaseSimpleLogin) {
+	
+	//use for user logining 
+	var ref = new Firebase(url + "/Users");
+	$rootScope.auth = $firebaseSimpleLogin(ref);
 
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+	//function to run when user logout 
+	$scope.logout = function(){
+		$rootScope.auth.$logout();
+		$location.path("");
+	}
+	//upload user information when first visit and create message listener 
+}])
+.controller('BuyCtrl', ["$scope", "$rootScope", "$firebase", function($scope, $rootScope, $firebase){
+	
+}]);
